10dcf9e52002f5e48bdbbbe1f9429b2bc383c547
skarang1 <skarang1@asu.edu>
2026-04-06 20:23:04 -0700

    cleaning up the root directory

--- src/Components/Part.jsx
+++
@@ -1,7 +0,0 @@
-const Part = ({content})=>{
-    return (
-        <p>{content.name} {content.exercises}</p>
-    )
-}
-
-export default Part
--- src/Components/Total.jsx
+++
@@ -1,8 +0,0 @@
-const Total = ({parts})=>{
-    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-    return (
-        <p><b>Total of {total} exercises</b></p>
-    )       
-}
-
-export default Total
