398a4fb660444637b9e706b0071a26c81ec978d8
skarang1 <skarang1@asu.edu>
2026-05-27 21:00:38 -0700

    cleaned the repo

--- src/components/SearchCountry.jsx
+++
@@ -1,9 +0,0 @@
-const SearchCountry = (props) => {
-    return (
-    <div>
-        find countries <input value={props.SearchValue} onChange={props.onChange}/>
-    </div>
-    )
-}
-
-export default SearchCountry
