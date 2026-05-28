7c7ae3fd1a3d4912913a7703537764825a9ddd62
skarang1 <skarang1@asu.edu>
2026-05-27 20:19:02 -0700

    exercise 2.20

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
-import countriesData from './services/countriesData'
+import countriesServices from './services/countriesServices'
 import SearchCountry from './components/SearchCountry'
 import DisplayCountries from './components/DisplayCountries'
 import DisplayOneCountry from './components/DisplayOneCountry'
 
 function App() {
-  const [countryList,setCountryList] = useState(null) 
+  const [countryList,setCountryList] = useState([]) 
   const [searchCountry, setSearchCountry] = useState('') 
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [weatherData, setWeatherData] = useState(null)
+  const [windSpeed, setWindSpeed] = useState(null)
+  const [iconCode, setIconCode] = useState(null)
 
   useEffect(()=>{
-    countriesData
+    countriesServices
     .getAllCountries()
     .then(allCountries => {
       console.log(`promise fulfilled-getAllCountries`)
@@ -20,15 +23,36 @@ function App() {
     })
   },[])
 
-  
-  const handleSearchCountry = (event) => {
-    setSearchCountry(event.target.value)
+  const computeWeatherData = (lat,long) => {
+    countriesServices
+    .getWeatherData(lat,long)
+    .then(data => {
+      console.log(`promise fulfilled-getWeatherData`)
+      console.log(`weather data is ${data.main.temp} and wind speed is ${data.wind.speed}`)
+      setWeatherData(data.main.temp)
+      setWindSpeed(data.wind.speed)
+      setIconCode(data.weather[0].icon)
+    }).catch(error => console.log(`Error occurred while fetching weather data: ${error}`))
+  }
 
-    let filteredListOfCountries  = countryList ? countryList.filter(country => 
-    country.name.common.toLowerCase().includes(searchCountry.toLowerCase())) : []
+  const handleSearchCountry = (event) => {
+    const searchValue = event.target.value
+    setSearchCountry(searchValue)
+
+    let filteredListOfCountries  = countryList.filter(country => 
+    country.name.common.toLowerCase().includes(searchValue.toLowerCase()))
 
     setFilteredCountries(filteredListOfCountries)
     console.log(`filtered list of countries is ${filteredListOfCountries}`)
+    if(filteredListOfCountries.length === 1){
+      const lat = filteredListOfCountries[0].latlng[0]
+      const lon = filteredListOfCountries[0].latlng[1]
+      console.log(`lat is ${lat} and lon is ${lon}`)
+      computeWeatherData(lat,lon)
+    }else{
+      setWeatherData(null)
+      setWindSpeed(null)  
+    }
 
   }
 
@@ -37,6 +61,10 @@ function App() {
     const countryName = event.target.parentNode.firstChild.textContent 
     const countryToBeShown = countryList.find(country => country.name.common === countryName)
     setFilteredCountries([countryToBeShown])
+    const lat = countryToBeShown.latlng[0]
+    const lon = countryToBeShown.latlng[1]
+    console.log(`lat is ${lat} and lon is ${lon}`)
+    computeWeatherData(lat,lon)
   }
 
 
@@ -44,7 +72,7 @@ function App() {
     <div>
       <SearchCountry searchValue={searchCountry} onChange={handleSearchCountry}/>
       {filteredCountries.length === 1 ? 
-       <DisplayOneCountry country={filteredCountries[0]}/> : 
+       <DisplayOneCountry country={filteredCountries[0]} temperature={weatherData} wind={windSpeed} iconCodeImg={iconCode}/> : 
        <DisplayCountries countryList = {filteredCountries} searchValue={searchCountry} onClickShow={handleShowButton}/>} 
     </div>
   )
